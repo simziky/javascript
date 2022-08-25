3743557e24d14124685aed1f04139b9fac68a1e7
simeon ehioze <simeonehioze@gmail.com>
2022-08-25 22:28:22 +0100

    first coding challenge

---
+++ Bmi.js
@@ -0,0 +1,21 @@
+/*
+var MarkMass = 50;
+var MarkHeight = 1.4;
+
+var MarkBmi = MarkMass / (MarkHeight * MarkHeight);
+console.log(MarkBmi)
+
+var JohnMass = 80;
+var JohnHeight = 1.5;
+var JohnBmi = JohnMass / (JohnHeight * JohnHeight);
+console.log(JohnBmi)
+
+var highestBmi = MarkBmi > JohnBmi;
+
+if (MarkBmi > JohnBmi) {
+    console.log(' mark\'s BMI is higher than john\'s');
+}else {
+    console.log(' john\'s  BMI is higher than mark\'s');
+}
+
+*/
