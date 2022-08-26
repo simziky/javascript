322afe6ad519175551ecd8a3a9761e304c3f43c9
simeon ehioze <simeonehioze@gmail.com>
2022-08-26 20:28:29 +0100

    second coding challenge Calculating average B ball score

---
+++ Bscore.js
@@ -0,0 +1,20 @@
+/*
+var jTeamAverageScore = (89 + 120 + 103)/3;
+    console.log(jTeamAverageScore);
+
+var mTeamAverageScore = (116 + 94 +123)/3 ;
+    console.log(mTeamAverageScore);
+
+var mATeamAverageScore = (97 + 134 +105)/3 ;
+    console.log(mATeamAverageScore);
+
+     if (jTeamAverageScore > mTeamAverageScore && jTeamAverageScore >  mATeamAverageScore ){
+            console.log('John\'s team is the winner' +' ' + 'with a score of' + ' ' + jTeamAverageScore)
+    }else if (mTeamAverageScore > jTeamAverageScore && mTeamAverageScore > mATeamAverageScore){
+        console.log('Mike\'s team is the winner' +' ' + 'with a score of' + ' ' + mTeamAverageScore )
+    }else if (mATeamAverageScore > jTeamAverageScore && mATeamAverageScore >  mTeamAverageScore){
+        console.log('Mary\'s team is the winner' +' ' + 'with a score of' + ' ' + mATeamAverageScore )
+    }else {
+        console.log('it is a draw')
+    } ;
+    */
